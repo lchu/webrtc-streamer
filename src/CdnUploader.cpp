c6caed2c7824ad6271c8a23cc7b5ae1c1478965c
chulei <chu.lei@chinaott.net>
2025-03-21 16:42:23 +0800

    modify file call1

--- src/CdnUploader.cpp
+++ src/CdnUploader.cpp
@@ -1,11 +1,11 @@
 #include "CdnUploader.h"
 #include "rtc_base/logging.h"
 // Update FFmpeg includes to use proper directory structure
-#include "libavutil/avutil.h"
-#include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-#include "libswscale/swscale.h"
-#include "libavutil/imgutils.h"
+#include "avutil.h"
+#include "avcodec.h"
+#include "avformat.h"
+#include "swscale.h"
+#include "imgutils.h"
 
 // FFmpeg资源
 AVFormatContext* m_outputContext = nullptr;
